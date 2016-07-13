1c18fad696bf5b37555046a1dd3384d751633ac7
Cdiez50 <c.deret@hotmail.fr>
2016-07-13 10:02:48 +0200

    update tmp path

--- actions/batterie_alerte.sh
+++ actions/batterie_alerte.sh
@@ -3,19 +3,19 @@
 
 # Si le retour de la commande suivante est inférieure à 20
 a=$(acpi | tr ' ' ':' | cut -d ':' -f 5 | tr -d '%,')
-b="20"
+b="111"
 
 	if [ "$a" -lt "$b" ]  
 		
 	then	
 		# On demande à LN de nous alerter sur la batterie :
-		sudo pico2wave -l fr-FR -w test.wav "Message Système. Attention, il reste ` acpi | grep -o '\w*%'` de batterie"
+		sudo pico2wave -l fr-FR -w ../tmp/test.wav "Message Système. Attention, il reste ` acpi | grep -o '\w*%'` de batterie"
 
 		# On lit le retour enregistré 
-		play test.wav
+		play ../tmp/test.wav
 
 		# On supprime le retour 
-		rm -f test.wav
+		rm -f ../tmp/test.wav
 	
 	else
 		echo "ok" > /dev/null 		
